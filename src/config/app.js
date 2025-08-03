fc6c719a5e4d2a22242b62c302b1ab960a88d520
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-08-03 12:10:01 +0100

    k

--- src/config/app.js
+++ src/config/app.js
@@ -7,23 +7,11 @@ const path = require('path');
 
 const app = express();
 
-// Middlewares
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://dev-kings-youssefbellouzwebs-projects.vercel.app/'
-];
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true, // or use '*', but only if you don't need credentials
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, '..', '..', 'uploads')));
