fcfbd083ad837816efcdf3bad1a040cefb477fb8
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-08-05 21:18:05 +0100

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://dev-kings.vercel.app',
-  'https://dev-front-omega.vercel.app'
+  'https://devkingfrontend.vercel.app'
 
 ];
 
